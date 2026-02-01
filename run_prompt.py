ecf3e35cf7b02e94ab2312bb0a13ffa8ef225f18
Chandanakondapalli <kondapallichandana0@gmail.com>
2026-02-01 12:40:38 +0530

    Fix import and update model usage for Google GenAI in run_prompt.py; correct package name in requirements.txt

--- run_prompt.py
+++ run_prompt.py
@@ -1,18 +1,19 @@
 import base64
 import os
-import google.generativeai as genai
+from google import genai
 
 # To run this code you need to install the following dependencies:
-# pip install google-generativeai
+# pip install google-genai
 
 GEMINI_API_KEY = "AIzaSyBD0IMoue8bUXotzPY3z3SzBrcXf5R1Vwo"
 
 def execute_gemini(prompt):
-    genai.configure(api_key=GEMINI_API_KEY)
+    client = genai.Client(api_key=GEMINI_API_KEY)
     
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    
-    response = model.generate_content(prompt)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=prompt
+    )
     
     return response.text
 
@@ -29,11 +30,12 @@ if __name__ == "__main__":
 
 
 def execute_gemini_for_tweets(prompt):  # INFO: THIS IS FOR TWEET CREATION
-    genai.configure(api_key=GEMINI_API_KEY)
+    client = genai.Client(api_key=GEMINI_API_KEY)
     
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    
-    response = model.generate_content(prompt)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=prompt
+    )
     
     return response.text
 
